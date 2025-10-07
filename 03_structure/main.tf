0725710a38814504201ff0005c37bb9f86b6b72f
ssfufu <mathieu.medinas@gmail.com>
2025-10-07 14:37:59 +0000

    Exercice 3.1  Structure du projet Terraform
    Signed-off-by: ssfufu <mathieu.medinas@gmail.com>

---
+++ 03_structure/main.tf
@@ -0,0 +1,23 @@
+terraform {
+ required_providers {
+   docker = {
+     source  = "kreuzwerker/docker"
+     version = "3.6.2"
+   }
+ }
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = true
+}
+
+resource "docker_container" "nginx" {
+  name    = "nginx"
+  image   = docker_image.nginx.image_id
+
+  ports {
+    external = var.container_port
+    internal = 80
+  }
+}
---
+++ 03_structure/variables.tf
@@ -0,0 +1,5 @@
+variable "container_port" {
+  description	= "Port exposé par Docker"
+  type		= number
+  default	= 9090
+}
